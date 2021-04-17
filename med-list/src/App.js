afd4d555a2ef719b7ec0bda0d83bb68fcef5d48c
Jakeas <JakeASierra@gmail.com>
2021-04-17 14:17:43 -0400

    worked on form - still needs functionality

--- med-list/src/App.js
+++ med-list/src/App.js
@@ -1,9 +1,11 @@
+import React from 'react'
+import WeekForm from './components/WeekForm'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <WeekForm />
     </div>
   );
 }
--- med-list/src/components/weekForm.js
+++ med-list/src/components/weekForm.js
@@ -1,34 +1,128 @@
 import React, { useState } from 'react'
 
-const weekForm = () => {
-    const [values, setValues] = useState()
+const WeekForm = () => {
+   const [selectedOption, setSelectedOption] = useState('select')
 
     const handleChange = (e) => {
-        setValues({
-            ...values, 
-            [e.target.name]: e.target.value})
-    }
-    
-    const addButtonHandler = (e) => {
-        e.preventDefault()
-    }
+      setSelectedOption({value: e.target.value})
+   }
+   const handleMedList = (e) => {
+      e.preventDefault()
+   }
 
-    return (
-        <div>
-            <h1>Daily Medication</h1>
-            <form>
-                <h2>Sunday</h2>
-                <label> AM
-                    <select value={values} onChange={handleChange}>
-                        <option>---Taken?---</option>
-                        <option>Yes</option>
-                        <option>No</option>
-                    </select>
-                </label>
-            </form>
-        </div>
-
-)
+   return (
+      <div>
+         <h1>Daily Medication</h1>
+         <form>
+               <h2>Sunday</h2>
+               <label> AM
+                  <select value={selectedOption} onChange={handleChange}>
+                     <option value="Taken?">---Taken?---</option>
+                     <option value="Yes">Yes</option>
+                     <option value="No">No</option>
+                  </select>
+               </label>
+               <label> PM
+                  <select value={selectedOption} onChange={handleChange}>
+                     <option>---Taken?---</option>
+                     <option>Yes</option>
+                     <option>No</option>
+                  </select>
+               </label>
+               <h2>Monday</h2>
+               <label> AM
+                  <select value={selectedOption} onChange={handleChange}>
+                     <option>---Taken?---</option>
+                     <option>Yes</option>
+                     <option>No</option>
+                  </select>
+               </label>
+               <label> PM
+                  <select value={selectedOption} onChange={handleChange}>
+                     <option>---Taken?---</option>
+                     <option>Yes</option>
+                     <option>No</option>
+                  </select>
+               </label>
+               <h2>Tuesday</h2>
+               <label> AM
+                  <select value={selectedOption} onChange={handleChange}>
+                     <option>---Taken?---</option>
+                     <option>Yes</option>
+                     <option>No</option>
+                  </select>
+               </label>
+               <label> PM
+                  <select value={selectedOption} onChange={handleChange}>
+                     <option>---Taken?---</option>
+                     <option>Yes</option>
+                     <option>No</option>
+                  </select>
+               </label>
+               <h2>Wednesday</h2>
+               <label> AM
+                  <select value={selectedOption} onChange={handleChange}>
+                     <option>---Taken?---</option>
+                     <option>Yes</option>
+                     <option>No</option>
+                  </select>
+               </label>
+               <label> PM
+                  <select value={selectedOption} onChange={handleChange}>
+                     <option>---Taken?---</option>
+                     <option>Yes</option>
+                     <option>No</option>
+                  </select>
+               </label>
+               <h2>Thursday</h2>
+               <label> AM
+                  <select value={selectedOption} onChange={handleChange}>
+                     <option>---Taken?---</option>
+                     <option>Yes</option>
+                     <option>No</option>
+                  </select>
+               </label>
+               <label> PM
+                  <select value={selectedOption} onChange={handleChange}>
+                     <option>---Taken?---</option>
+                     <option>Yes</option>
+                     <option>No</option>
+                  </select>
+               </label>
+               <h2>Friday</h2>
+               <label> AM
+                  <select value={selectedOption} onChange={handleChange}>
+                     <option>---Taken?---</option>
+                     <option>Yes</option>
+                     <option>No</option>
+                  </select>
+               </label>
+               <label> PM
+                  <select value={selectedOption} onChange={handleChange}>
+                     <option>---Taken?---</option>
+                     <option>Yes</option>
+                     <option>No</option>
+                  </select>
+               </label>
+               <h2>Saturday</h2>
+               <label> AM
+                  <select value={selectedOption} onChange={handleChange}>
+                     <option>---Taken?---</option>
+                     <option>Yes</option>
+                     <option>No</option>
+                  </select>
+               </label>
+               <label> PM
+                  <select value={selectedOption} onChange={handleChange}>
+                     <option>---Taken?---</option>
+                     <option>Yes</option>
+                     <option>No</option>
+                  </select>
+               </label>
+               <button onChange={handleMedList}>Med List</button>
+         </form>
+      </div>
+   )
 }
 
-export default weekForm
+export default WeekForm
